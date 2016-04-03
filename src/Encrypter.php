547bd78cd8f08317fc171039ab37a6ee58d0d12c
Florent Morselli <florent+git@morselli.fr>
2016-04-03 23:50:42 +0200

    PhpDoc

--- src/Encrypter.php
+++ src/Encrypter.php
@@ -52,10 +52,10 @@ final class Encrypter implements EncrypterInterface
     /**
      * Decrypter constructor.
      *
-     * @param string[]|\Jose\Algorithm\JWAInterface[]           $key_encryption_algorithms
-     * @param string[]|\Jose\Algorithm\JWAInterface[]           $content_encryption_algorithms
-     * @param string[]|\Jose\Compression\CompressionInterface[] $compression_methods
-     * @param \Psr\Log\LoggerInterface|null                     $logger
+     * @param string[]|\Jose\Algorithm\KeyEncryptionAlgorithmInterface[]     $key_encryption_algorithms
+     * @param string[]|\Jose\Algorithm\ContentEncryptionAlgorithmInterface[] $content_encryption_algorithms
+     * @param string[]|\Jose\Compression\CompressionInterface[]              $compression_methods
+     * @param \Psr\Log\LoggerInterface|null                                  $logger
      */
     public function __construct(
         array $key_encryption_algorithms,
