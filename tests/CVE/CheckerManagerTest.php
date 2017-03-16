af3a88fc33eb51fde812ee42431cfa8fe2fdf35c
Florent Morselli <florent@morselli.fr>
2017-03-16 22:05:51 +0100

    Tests added to verify the library is immune against the CVE-2015-7940
    See https://access.redhat.com/security/cve/CVE-2015-7940

---
+++ tests/CVE/CheckerManagerTest.php
@@ -0,0 +1,64 @@
+<?php
+
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014-2016 Spomky-Labs
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+use Jose\Decrypter;
+use Jose\Loader;
+use Jose\Test\TestCase;
+
+/**
+ * @group CVE
+ * @group Functional
+ */
+class InvalidCurveAttackTest extends TestCase
+{
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage Unable to decrypt the JWE.
+     */
+    public function testCurveCheckNegativeP256AttackPt1()
+    {
+        $maliciousJWE = "eyJhbGciOiJFQ0RILUVTK0ExMjhLVyIsImVuYyI6IkExMjhDQkMtSFMyNTYiLCJlcGsiOnsia3R5IjoiRUMiLCJ4IjoiZ1RsaTY1ZVRRN3otQmgxNDdmZjhLM203azJVaURpRzJMcFlrV0FhRkpDYyIsInkiOiJjTEFuakthNGJ6akQ3REpWUHdhOUVQclJ6TUc3ck9OZ3NpVUQta2YzMEZzIiwiY3J2IjoiUC0yNTYifX0.qGAdxtEnrV_3zbIxU2ZKrMWcejNltjA_dtefBFnRh9A2z9cNIqYRWg.pEA5kX304PMCOmFSKX_cEg.a9fwUrx2JXi1OnWEMOmZhXd94-bEGCH9xxRwqcGuG2AMo-AwHoljdsH5C_kcTqlXS5p51OB1tvgQcMwB5rpTxg.72CHiYFecyDvuUa43KKT6w";
+        $decrypter = Decrypter::createDecrypter(['ECDH-ES+A128KW'], ['A128CBC-HS256']);
+
+        $loader = new Loader();
+        $loaded_compact_json = $loader->load($maliciousJWE);
+        $privateKey = \Jose\Factory\JWKFactory::createECKey([
+            'kty' => 'EC',
+            'crv' => 'P-256',
+            "x" => "weNJy2HscCSM6AEDTDg04biOvhFhyyWvOHQfeF_PxMQ",
+            "y" => "e8lnCO-AlStT-NJVX-crhB7QRYhiix03illJOVAOyck",
+            "d" => "VEmDZpDXXK8p8N0Cndsxs924q6nS1RXFASRl6BfUqdw",
+        ]);
+        $decrypter->decryptUsingKey($loaded_compact_json, $privateKey);
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage Unable to decrypt the JWE.
+     */
+    public function testCurveCheckNegativeP256AttackPt2()
+    {
+        // The malicious JWE contains a public key with order 2447
+        $maliciousJWE = "eyJhbGciOiJFQ0RILUVTK0ExMjhLVyIsImVuYyI6IkExMjhDQkMtSFMyNTYiLCJlcGsiOnsia3R5IjoiRUMiLCJ4IjoiWE9YR1E5XzZRQ3ZCZzN1OHZDSS1VZEJ2SUNBRWNOTkJyZnFkN3RHN29RNCIsInkiOiJoUW9XTm90bk56S2x3aUNuZUprTElxRG5UTnc3SXNkQkM1M1ZVcVZqVkpjIiwiY3J2IjoiUC0yNTYifX0.UGb3hX3ePAvtFB9TCdWsNkFTv9QWxSr3MpYNiSBdW630uRXRBT3sxw.6VpU84oMob16DxOR98YTRw.y1UslvtkoWdl9HpugfP0rSAkTw1xhm_LbK1iRXzGdpYqNwIG5VU33UBpKAtKFBoA1Kk_sYtfnHYAvn-aes4FTg.UZPN8h7FcvA5MIOq-Pkj8A";
+        $decrypter = Decrypter::createDecrypter(['ECDH-ES+A128KW'], ['A128CBC-HS256']);
+
+        $loader = new Loader();
+        $loaded_compact_json = $loader->load($maliciousJWE);
+        $privateKey = \Jose\Factory\JWKFactory::createECKey([
+            'kty' => 'EC',
+            'crv' => 'P-256',
+            "x" => "weNJy2HscCSM6AEDTDg04biOvhFhyyWvOHQfeF_PxMQ",
+            "y" => "e8lnCO-AlStT-NJVX-crhB7QRYhiix03illJOVAOyck",
+            "d" => "VEmDZpDXXK8p8N0Cndsxs924q6nS1RXFASRl6BfUqdw",
+        ]);
+        $decrypter->decryptUsingKey($loaded_compact_json, $privateKey);
+    }
+}
